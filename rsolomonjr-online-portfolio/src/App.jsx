5ac593bd38b2993ebe1689fae8fb287128fb6b59
rsolomon <rsolomon@webmd.net>
2023-11-20 22:52:39 -0500

    update portfolio

--- rsolomonjr-online-portfolio/src/App.jsx
+++ rsolomonjr-online-portfolio/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Rodney Solomon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <nav className="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          RS
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="collapse navbar-collapse" id="navbarPortfolio">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a href="#" className="nav-link">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a href="#" className="nav-link">
+                Recommendations
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a href="#" className="nav-link">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
