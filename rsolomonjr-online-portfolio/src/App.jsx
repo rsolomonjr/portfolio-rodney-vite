16581a1863f265d27a8c53d0aca3a4e3629400e5
rsolomon <rsolomon@webmd.net>
2023-11-22 00:15:36 -0500

    latest-nightly

--- rsolomonjr-online-portfolio/src/App.jsx
+++ rsolomonjr-online-portfolio/src/App.jsx
@@ -8,48 +8,25 @@ function App() {
 
   return (
     <>
-      <h1>Rodney Solomon</h1>
-      <nav className="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          RS
-        </a>
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarPortfolio">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Projects
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Recommendations
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Blog
-              </a>
-            </li>
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <main>
+    <h1>Rodney Solomon</h1>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">RS</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Projects</a>
+        <a className="nav-link" href="#">Recommendations</a>
+        <a className="nav-link" href="#">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    </main>
+     
     </>
   );
 }
