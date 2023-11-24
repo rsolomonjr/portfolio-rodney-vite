645f177ffdc3b62af002facb504d9bf28c439d7c
rsolomon <rsolomon@webmd.net>
2023-11-24 01:57:00 -0500

    fixed scroll to top

--- rsolomonjr-online-portfolio/src/App.jsx
+++ rsolomonjr-online-portfolio/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window > 300) {
+      if (window.scrollY > 300) {
         setShowButton(true);
       } else {
         setShowButton(false);
